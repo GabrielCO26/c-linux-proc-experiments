b2dad43518ac5a86bb6999d39d7cf989cecb3b55
Gabriel Castro <130525696+GabrielCO26@users.noreply.github.com>
2025-04-30 21:03:11 -0500

    sumasec.c output is now shown in order

--- sumasec.c
+++ sumasec.c
@@ -3,27 +3,33 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
   int n = atoi(argv[1]);
-  pid_t pid;
-  for (int i = 0; i < n; i++){
-    pid = fork();
-    if (pid == 0){
-      int fin = 2*(i+1)-1;
-      int suma = 0;
-      printf("Listado de números impresos por proceso %d\n", getpid());
-      for (int j = i; j <= fin; j++){
-        printf("Proceso %d, secuencia %d\n", getpid(), j);
-        suma += j;
-      }
+  pid_t pids[n];
+  int resultados[n];
+  
+  for (int i = 0; i < n; i++) {
+      pid_t pid = fork();
+      if (pid == 0) { // Proceso hijo
+          int fin = 2 * (i + 1) - 1;
+          int suma = 0;
+          printf("Listado de números impresos por proceso %d\n", getpid());
+          for (int j = i; j <= fin; j++) {
+              printf("Proceso %d, secuencia %d\n", getpid(), j);
+              suma += j;
+          }
       exit(suma);
-    }
+      } else { // Proceso padre
+          int status;
+          waitpid(pid, &status, 0);
+          pids[i] = pid;
+          resultados[i] = WEXITSTATUS(status);
+      }
   }
-  sleep(1);
-  for (int i = 0; i < n; i++){
-    int status;
-    pid_t pid_hijo = wait(&status);
-    printf("Hijo %d terminó con suma: %d\n", pid_hijo, WEXITSTATUS(status));
+
+  for (int i = 0; i < n; i++) {
+      printf("Hijo con PID %d terminó con suma: %d\n", pids[i], resultados[i]);
   }
-return 0;
+  return 0;
 }
+
